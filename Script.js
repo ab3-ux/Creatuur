47c176fdbfdb417f4976dc5a00e65b7493ab7f92
A-Beeg.O <80334438+ab3-ux@users.noreply.github.com>
2026-03-31 14:06:27 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,12 @@
 
+function openBarrel() {
+  const barrel = document.getElementById("barrel");
+
+  // 🫨 effet tremblement
+  barrel.style.transform = "scale(1.1)";
+  
+  // 💥 simulation explosion
+  setTimeout(() => {
+    barrel.src = "images/creature.jpg";
+  }, 1000);
+}
