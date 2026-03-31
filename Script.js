05b16ab893c646ad4f4978e58957d1a6a0c26e13
A-Beeg.O <80334438+ab3-ux@users.noreply.github.com>
2026-03-31 17:22:50 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,12 +1,5 @@
-
 function openBarrel() {
   const barrel = document.getElementById("barrel");
 
-  // 🫨 effet tremblement
-  barrel.style.transform = "scale(1.1)";
-  
-  // 💥 simulation explosion
-  setTimeout(() => {
-    barrel.src = "images/Creatur001.png";
-  }, 1000);
+  barrel.src = "images/creature.jpg";
 }
