7b486a7fc76e029d9959642ea3bfeb12661ca391
A-Beeg.O <80334438+ab3-ux@users.noreply.github.com>
2026-04-01 23:42:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-function openBarrel() {
-  const barrel = document.getElementById("barrel");
 
-  // effet visuel
-  barrel.style.border = "5px solid red";
-  barrel.style.transform = "scale(1.1)";
-  barrel.style.filter = "brightness(1.5)";
-
-  // retour normal
-  setTimeout(() => {
-    barrel.style.border = "none";
-    barrel.style.transform = "scale(1)";
-    barrel.style.filter = "none";
-  }, 300);
-}
