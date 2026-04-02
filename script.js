4f77de45e3d5954506a9a7151d26d7c2a64dccad
A-Beeg.O <80334438+ab3-ux@users.noreply.github.com>
2026-04-02 15:36:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function openBarrel() {
+  let img = document.getElementById("barrel");
 
+  img.style.transform = "scale(1.2) rotate(3deg)";
+  img.style.filter = "drop-shadow(0 0 40px red)";
+
+  setTimeout(() => {
+    img.style.transform = "scale(1) rotate(0deg)";
+    img.style.filter = "drop-shadow(0 0 25px cyan)";
+  }, 300);
+}
